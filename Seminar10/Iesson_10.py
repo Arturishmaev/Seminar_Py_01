8b519e3d7cadbb5449b132debaf715f093f2fe9b
ArturIshmaev <arturishmaev@yandex.ru>
2023-03-28 17:59:56 +0500

    last commit

---
+++ Seminar10/Iesson_10.py
@@ -0,0 +1,7 @@
+class Human:
+    def __init__(self, name: str, age: int, is_work: bool):
+        self.name = name
+        self.age = age
+        self.is_work = is_work
+        
+stone = Human('Kirill', 38, True)
